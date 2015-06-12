d6cf47cd6fd99059ca788d82edd93a04e42614e0
benjamin.c.yan <benjamin.c.yan>
2015-06-12 17:24:02 +0800

    changed config

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@ BASE="$(cd `dirname $0`; pwd)"
 
 CONFIG="-sname ddrt -setcookie ddrt_dev +K true -kernel error_logger silent -pa $BASE/ebin -pa $BASE/deps/neg_hydra/ebin -s neg_hydra"
 
+echo "erl "$CONFIG" -config "$BASE"/config/sys.config"
 erl $CONFIG -config "$BASE/config/sys.config"
