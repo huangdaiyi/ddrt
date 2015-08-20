b14bf2fed13bb229e12efd4b8ec74a849927a1c2
tj97 <Tristan.T.Jiang@newegg.com>
2015-08-20 14:27:55 +0800

    fix

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 BASE="$(cd `dirname $0`; pwd)"
 
-CONFIG="-sname ddrt -setcookie ddrt_dev +K true -kernel error_logger silent -pa $BASE/ebin -pa $BASE/deps/neg_hydra/ebin -s neg_hydra"
+CONFIG=" -noshell -sname ddrt -setcookie ddrt_dev +K true -kernel error_logger silent -pa $BASE/ebin -pa $BASE/deps/neg_hydra/ebin -s neg_hydra"
 
 echo "erl "$CONFIG" -config "$BASE"/config/sys.config"
-erl $CONFIG -config "$BASE/config/sys.config" -noshell
+erl $CONFIG -config "$BASE/config/sys.config"
