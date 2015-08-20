3ed53f265c746764a9167b5303f29da5da609fd3
tj97 <Tristan.T.Jiang@newegg.com>
2015-08-20 18:52:56 +0800

    fix

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 
-CONFIG="-detached  -sname ddrt -setcookie ddrt_dev +K true -kernel error_logger silent -pa /ddrt/ebin -pa /ddrt/deps/neg_hydra/ebin -s neg_hydra"
+CONFIG="-noshell  -sname ddrt -setcookie ddrt_dev +K true -kernel error_logger silent -pa /ddrt/ebin -pa /ddrt/deps/neg_hydra/ebin -s neg_hydra"
 
 echo "erl "$CONFIG" -config  /ddrt/config/sys.config"
 erl $CONFIG -config "/ddrt/config/sys.config"
